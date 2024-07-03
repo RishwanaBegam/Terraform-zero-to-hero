2f3412f382555220e95cf6ae3884a449f7e18424
RishwanaBegam <149790091+RishwanaBegam@users.noreply.github.com>
2024-07-03 11:04:35 +0530

    Delete Day-1/PROJECT-ec2-instance-creation directory

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
-}
-
-resource "aws_instance" "example" {
-  ami                     = "ami-0dcc1e21636832c5d"   # Specify an appropriate AMI ID
-  instance_type           = "t2.micro"
-}
