476b3e4c0238b941afb5b6a3faf89d10b0fb47e1
RishwanaBegam <149790091+RishwanaBegam@users.noreply.github.com>
2024-07-04 13:06:37 +0530

    Create variables.tf

---
+++ DAY-3/Modules/ec2-instance/variables.tf
@@ -0,0 +1,15 @@
+variable "instance_type_value" {
+    description = "This is instance type value"
+}
+
+variable "ami_value" {
+    description = "This is ami id value"
+}
+
+variable "subnet_id_value" {
+    description = "This is subnet id value"
+}
+
+variable "name_value" {
+  description = "The name tag for the instance"
+}
