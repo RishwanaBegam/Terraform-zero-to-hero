130a223245fc86ff133d003df0d854351994864c
RishwanaBegam <149790091+RishwanaBegam@users.noreply.github.com>
2024-07-08 13:17:42 +0530

    Create app.py

---
+++ DAY-5/PROJECT-VPC-WITH-INSTANCE/app.py
@@ -0,0 +1,8 @@
+# This is a simple Python script to print "Hello, World!" to the console.
+
+def main():
+    print("Hello, Terraform!")
+
+if __name__ == "__main__":
+    main()
+    app.run(host="0.0.0.0", port=80)
