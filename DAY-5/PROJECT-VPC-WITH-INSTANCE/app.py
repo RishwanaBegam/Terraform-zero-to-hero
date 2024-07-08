7ddabb873a33f55526de592857b5ed3a9b10897a
RishwanaBegam <149790091+RishwanaBegam@users.noreply.github.com>
2024-07-08 13:21:40 +0530

    Update app.py

--- DAY-5/PROJECT-VPC-WITH-INSTANCE/app.py
+++ DAY-5/PROJECT-VPC-WITH-INSTANCE/app.py
@@ -1,8 +1,12 @@
 # This is a simple Python script to print "Hello, World!" to the console.
 
-def main():
-    print("Hello, Terraform!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, Terraform!"
 
 if __name__ == "__main__":
-    main()
     app.run(host="0.0.0.0", port=80)
